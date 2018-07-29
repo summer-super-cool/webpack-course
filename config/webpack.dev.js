c695d0a65d2bddea8c3937c4220accba2689f9c9
Summer <summer-super-cool@github.com>
2018-07-30 01:22:06 +0800

    加入babelrc 转化ES6

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -19,6 +19,16 @@ module.exports = {
     },
     module:{
         rules:[
+            //js loaders
+            {
+              test:/\.js$/,
+              use:[
+                {
+                  loader:"babel-loader"
+                }
+              ],
+              exclude:/node_modules/
+            },
             //css loaders
             {
                 test: /\.css$/,
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 require("./main.css")
 require("./index.html")
 // alert("hello world!!!");
+
+var a = () => {
+  console.log("Hello Future!")
+}
