ad31b290f567f1367d2db72119b934adfbb1d4b2
Summer <summer-super-cool@github.com>
2018-07-30 00:43:51 +0800

    基础环境上传

---
+++ config/webpack.dev.js
@@ -0,0 +1,69 @@
+const path = require("path")
+module.exports = {
+    //入口：有并且可以有多个
+    entry:{
+        main:["./src/main.js"]
+    },
+    // 打包环境：开发 & 生产
+    mode:"development",
+    // 出口：有且只能有一个
+    output:{
+        filename:"[name]-bundle.js",
+        path: path.resolve(__dirname,"../dist"),
+        publicPath:"/"
+    },
+    //本地服务器
+    devServer:{
+        contentBase:"dist",
+        overlay:true
+    },
+    module:{
+        rules:[
+            //css loaders
+            {
+                test: /\.css$/,
+                use:[
+                    {
+                        loader:"style-loader"
+                    },
+                    {
+                        loader:"css-loader"
+                    }
+                ]
+            },
+            //html loaders
+            {
+                test: /\.html$/,
+                use:[
+                    {
+                        loader:"file-loader",
+                        options:{
+                            name:"[name].html"
+                        }
+                    },
+                    {
+                        loader:"extract-loader"
+                    },
+                    {
+                        loader:"html-loader",
+                        options:{
+                            attrs:["img:src"]
+                        }
+                    }
+                ]
+            },
+          //image loaders
+          {
+            test: /\.(jpg|git|png)$/,
+            use:[
+                {
+                    loader:"file-loader",
+                    options:{
+                        name:"images/[name].[ext]"
+                    }
+                }
+            ]
+        }
+        ]
+    }
+}
